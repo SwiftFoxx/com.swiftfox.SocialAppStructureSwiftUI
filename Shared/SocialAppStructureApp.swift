12fc7733e1d7027fd5e01306705e826a4e674d45
Koushik Mudi <km.kaushik92@gmail.com>
2020-07-18 10:43:56 +0530

    Initial Commit

---
+++ Shared/SocialAppStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAppStructureApp.swift
+//  Shared
+//
+//  Created by Koushik Mudi on 18/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SocialAppStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
